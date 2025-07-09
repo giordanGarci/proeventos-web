97b4fcccac63e7cef197638aba234eaf52a86f3d
Giordan Garcia <giordangarcia12@gmail.com>
2025-07-09 00:05:05 -0300

    new context

---
+++ Back/src/ProEventos.Persistence/ProEventosContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.Domain;
+
+namespace ProEventos.Persistence;
+
+public class ProEventosContext : DbContext
+{
+
+    public ProEventosContext(DbContextOptions<ProEventosContext> options) : base(options) { }
+    public DbSet<Event> Events { get; set; }
+    public DbSet<Batch> Batcs { get; set; }
+    public DbSet<Speaker> Speakers { get; set; }
+    public DbSet<SpeakerEvent> SpeakersEvents { get; set; }
+    public DbSet<SocialMedia> SocialMedias { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<SpeakerEvent>()
+            .HasKey(se => new { se.EventId, se.SpeakerId });
+
+    }
+
+}
